26974d9d421115ad7c8635314014165d60cba101
Jorge Hortelano <jhortelano@biit-solutions.com>
2025-07-24 11:39:48 +0200

    Updating SecurityService.

--- form-results-rest/src/main/java/com/biit/forms/rest/api/FormResultsSecurityService.java
+++ form-results-rest/src/main/java/com/biit/forms/rest/api/FormResultsSecurityService.java
@@ -2,8 +2,12 @@ package com.biit.forms.rest.api;
 
 
 import com.biit.server.rest.SecurityService;
+import com.biit.server.security.IUserOrganizationProvider;
+import com.biit.server.security.model.IUserOrganization;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Primary
 @Service("securityService")
@@ -18,6 +22,10 @@ public class FormResultsSecurityService extends SecurityService {
     private String adminPrivilege = null;
     private String editorPrivilege = null;
     private String organizationAdminPrivilege = null;
+
+    public FormResultsSecurityService(List<IUserOrganizationProvider<? extends IUserOrganization>> userOrganizationProviders) {
+        super(userOrganizationProviders);
+    }
 
     @Override
     public String getViewerPrivilege() {
